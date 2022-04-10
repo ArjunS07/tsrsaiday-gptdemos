910ddaf3b517573210daa1d3ce83c54cba1b8674
Arjun Sharma <arjun.sharma07@outlook.com>
2022-04-11 00:26:31 +0530

    add profanity filter, add function to clean up story, fix homepage css

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+from string import punctuation
+
+from unsafe_words import is_safe
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
@@ -15,21 +18,49 @@ def index():
             request.form["story-prompt-1"],
             request.form["story-prompt-2"],
             request.form["story-prompt-3"],
-            request.form["story-prompt-4"]            
+            request.form["story-prompt-4"]
         ]
 
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=generate_prompt(words),
-            temperature=0.3,
-            max_tokens=200,
-            top_p=1,
-            best_of=1,
-            frequency_penalty=1.25,
-            presence_penalty=1
-        )
+        for word in words:
+            if not is_safe(word):
+                return render_template("index.html")
 
-        return render_template("index.html", anchor = "result", result = response.choices[0].text, words = words)
+        story_is_safe = False
+        story = ""
+
+        # test
+        test_var = 0
+
+        while not story_is_safe:
+            print('generating story...')
+
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=generate_prompt(words),
+                temperature=0.3,
+                max_tokens=200,
+                top_p=1,
+                best_of=1,
+                frequency_penalty=1.25,
+                presence_penalty=1
+            )
+
+            story = response.choices[0].text
+            story = clean_story(story)
+            print(story)
+
+            story_words = story.split()
+
+            story_is_safe = True
+
+            for story_word in story_words:
+                if not is_safe(story_word):
+                    story_is_safe = False
+            
+
+        
+        return render_template("index.html", anchor = "result", result = story, words = words)
+        
 
 
     # result = request.args.get("result")
@@ -39,8 +70,39 @@ def index():
 def generate_prompt(listOfWords):
 
     return """
-        Write a short story in 100 words or less using the following four words:\n\n1.{}\n2.{}\n3.{}\n4.{}",
+        Write a short story in 150 words or less using these four words:\n\n1.{}\n2.{}\n3.{}\n4.{}",
     """.format(
         listOfWords[0], listOfWords[1], listOfWords[2], listOfWords[3] 
     )
+
+def clean_story(story):
+    # Sometimes, GPT-3 gives a story with random punctuation at the end
+    # For example, it once generated: "I was walking my dog when I saw my adversary. It was a moment of epiphany. I realized that I had been pristine." ];
+    # This function removes the random punctuation at the end of the story
+
+    punctuation = [
+        '[',
+        ']',
+        ';',
+        ',',
+        '#',
+        '&',
+        "/",
+        "/"
+    ]
+
+    reversed = story[::-1]
+    
+    # Clean up ending punctuation
+    while reversed[0] in punctuation:
+        reversed = reversed[1:]
+    
+    if reversed[-1] == '"':
+        # Complete quotes, because GPT-3 sometimes likes to give the story with a trailing quote
+        reversed[0] = '"'
+
+    # unreverse it
+    return reversed[::-1]
+
+    
 
---
+++ unsafe_words.py
@@ -0,0 +1,18 @@
+def convert_lower(inp_list):
+    return list(map(lambda word: word.lower(), inp_list))
+
+import json
+unsafe_words = open('unsafe_words.json')
+data = json.load(unsafe_words)
+
+slurs_nsfw = convert_lower(data['slurs_and_nsfw'])
+drugs = convert_lower(data['drugs'])
+religions = convert_lower(data['religions'])
+races = convert_lower(data['races'])
+
+def is_safe(word):
+    lower = word.lower()
+    if lower in slurs_nsfw or lower in drugs or lower in religions or lower in races:
+        return False
+    else:
+        return True
